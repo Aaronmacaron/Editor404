edefd76b9d804b3ef7e3e48cb4a9a0a0c1ca73da
Aaron Ebnöther <aaron.ebnoether@gmail.com>
2017-03-19 08:48:42 +0100

    Remove files

--- src/me/aaronebnoether/SyntaxHighlighter/Highlighter.java
+++ src/me/aaronebnoether/SyntaxHighlighter/Highlighter.java
@@ -1,33 +1,5 @@
 package me.aaronebnoether.SyntaxHighlighter;
 
-import javafx.scene.text.Text;
-
-import java.util.ArrayList;
-
 public class Highlighter {
 
-    public static ArrayList<Text> highlight(String text) {
-        ArrayList<Word> words = Word.getWords(text);
-
-        ArrayList<Text> returnTexts = new ArrayList<>();
-        for (Word word : words) {
-            returnTexts.add(new Text(word.getText()));
-        }
-        return returnTexts;
-    }
-
-    private ArrayList<Text> getHighlightedTexts() {
-
-    }
-
-    private ArrayList<Token> getTokens(ArrayList<Word> words) {
-        ArrayList<Token> tokens = new ArrayList<Token>();
-        for (Word word : words) {
-            for (int i = 0; i < word.getText().length(); i++) {
-                if (TokenFamily.isSpecialCharacter(String.valueOf(word.getText().charAt(i)))) {
-                    tokens.add(new Token(String.valueOf(word.getText().charAt(i)), new TokenType()));
-                }
-            }
-        }
-    }
 }
