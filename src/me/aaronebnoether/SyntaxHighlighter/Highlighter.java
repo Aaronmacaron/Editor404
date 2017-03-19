e13d765c99e86a281a66cb79f38ececf2f506b17
Aaron Ebnöther <aaron.ebnoether@gmail.com>
2017-03-19 22:21:40 +0100

    Keywords, Strings and Comments work now

--- src/me/aaronebnoether/SyntaxHighlighter/Highlighter.java
+++ src/me/aaronebnoether/SyntaxHighlighter/Highlighter.java
@@ -9,105 +9,148 @@ import java.util.regex.Pattern;
 
 public class Highlighter {
 
+    private final static String KW_FLAG = "\\:k:\\";
+    private final static String KW_END_FLAG = "\\.k.\\";
+    private final static String ST_FLAG = "\\:s:\\";
+    private final static String ST_END_FLAG = "\\.s.\\";
+    private final static String NM_FLAG = "\\:n:\\";
+    private final static String NM_END_FLAG = "\\.n.\\";
+    private final static String CO_FLAG = "\\:c:\\";
+    private final static String CO_END_FLAG = "\\.c.\\";
+
     public static ArrayList<Text> texts(String text){
+        return getTextsOfHighlightedString(getHighlightedString(text));
+    }
+
+    public static ArrayList<Text> getTextsOfHighlightedString(String hlString) {
         ArrayList<Text> texts = new ArrayList<>();
-        for (Token token : getTokens(text)) {
-            Text textNode = new Text();
-            textNode.setText(token.getValue());
-            if (Pattern.matches("^KW.+", token.getTokenType())) {
-                textNode.setFill(Color.web("CC7832"));
+        String currentColor = "000000";
+        String typeExpected = NM_FLAG;
+        for (int i = 0; i < hlString.length(); i++) {
+            String flagRange = hlString.substring(i, i + 5 > hlString.length() ? i : i + 5);
+            switch (flagRange) {
+                case KW_FLAG: {
+                    if (typeExpected.equals(NM_FLAG)) {
+                        if (currentColor.equals("000000")) {
+                            currentColor = "CC7832";
+                        }
+                        typeExpected = KW_END_FLAG;
+                    }
+                    hlString = new StringBuilder(hlString).delete(i, i + 5).toString();
+                    break;
+                }
+                case ST_FLAG: {
+                    if (typeExpected.equals(NM_FLAG)) {
+                        if (currentColor.equals("000000")) {
+                            currentColor = "6A8759";
+                        }
+                        typeExpected = ST_END_FLAG;
+                    }
+                    hlString = new StringBuilder(hlString).delete(i, i + ST_FLAG.length()).toString();
+                    break;
+                }
+                case CO_FLAG: {
+                    if (typeExpected.equals(NM_FLAG)) {
+                        if (currentColor.equals("000000")) {
+                            currentColor = "629755";
+                        }
+                        typeExpected = CO_END_FLAG;
+                    }
+                    hlString = new StringBuilder(hlString).delete(i, i + CO_FLAG.length()).toString();
+                    break;
+                }
+                case KW_END_FLAG: {
+                    if (typeExpected.equals(KW_END_FLAG)) {
+                        currentColor = "000000";
+                        typeExpected = NM_FLAG;
+                    }
+                    hlString = new StringBuilder(hlString).delete(i, i + KW_FLAG.length()).toString();
+                    break;
+                }
+                case ST_END_FLAG: {
+                    if (typeExpected.equals(ST_END_FLAG)) {
+                        currentColor = "000000";
+                        typeExpected = NM_FLAG;
+                    }
+                    hlString = new StringBuilder(hlString).delete(i, i + ST_FLAG.length()).toString();
+                    break;
+                }
+                case CO_END_FLAG: {
+                    if (typeExpected.equals(CO_END_FLAG)) {
+                        currentColor = "000000";
+                        typeExpected = NM_FLAG;
+                    }
+                    hlString = new StringBuilder(hlString).delete(i, i + CO_FLAG.length()).toString();
+                    break;
+                }
+                case NM_FLAG: {
+                    currentColor = "000000";
+                    hlString = new StringBuilder(hlString).delete(i, i + 5).toString();
+                    break;
+                }
             }
-            if (Pattern.matches("^LI.+", token.getTokenType())) {
-                textNode.setFill(Color.web("6A8759"));
+            Text text = new Text();
+            if (i >= hlString.length()) {
+                text.setText("");
+            } else {
+                text.setText(String.valueOf(hlString.charAt(i)));
             }
-            if (Pattern.matches("^CO.+", token.getTokenType())) {
-                textNode.setFill(Color.web("629755"));
-            }
-            texts.add(textNode);
-            System.out.printf("Type: %s, Start: %s, End: %s, Value: %s\n", token.getTokenType(), token.getStartIndex(), token.getEndIndex(), token.getValue());
+            text.setFill(Color.web(currentColor));
+            texts.add(text);
         }
         return texts;
     }
 
-    public static ArrayList<Token> getTokens(String text) {
-        ArrayList<Token> tokens = new ArrayList<>();
+    public static String getHighlightedString(String text) {
         for (Rule rule : getRules()) {
             Pattern pattern = Pattern.compile(rule.getPattern());
             Matcher matcher = pattern.matcher(text);
+            int offset = 0;
             while (matcher.find()) {
-                tokens.add(new Token(matcher.group(), matcher.start(), matcher.end(), rule.getRuleName()));
+                switch (rule.getRuleName()) {
+                    case "KW": {
+                        StringBuilder stringBuilder = new StringBuilder(text).insert(matcher.start() + offset, KW_FLAG);
+                        offset += KW_FLAG.length();
+                        text = stringBuilder.toString();
+                        stringBuilder = new StringBuilder(text).insert(matcher.end() + offset, KW_END_FLAG);
+                        text = stringBuilder.toString();
+                        offset += NM_FLAG.length();
+                        break;
+                    }
+                    case "ST": {
+                        StringBuilder stringBuilder = new StringBuilder(text).insert(matcher.start() + offset, ST_FLAG);
+                        offset += ST_FLAG.length();
+                        text = stringBuilder.toString();
+                        stringBuilder = new StringBuilder(text).insert(matcher.end() + offset, ST_END_FLAG);
+                        text = stringBuilder.toString();
+                        offset += NM_FLAG.length();
+                        break;
+                    }
+                    case "CO": {
+                        StringBuilder stringBuilder = new StringBuilder(text).insert(matcher.start() + offset, CO_FLAG);
+                        offset += CO_FLAG.length();
+                        text = stringBuilder.toString();
+                        stringBuilder = new StringBuilder(text).insert(matcher.end() + offset, CO_END_FLAG);
+                        text = stringBuilder.toString();
+                        offset += NM_FLAG.length();
+                        break;
+                    }
+                }
             }
         }
-        tokens.sort((token1, token2) -> {
-            if (token1.getStartIndex() == token2.getStartIndex()) {
-                return 0;
-            } else if (token1.getStartIndex() < token2.getStartIndex()) {
-                return -1;
-            } else return 1;
-        });
-        return tokens;
+        return text;
     }
 
     private static ArrayList<Rule> getRules() {
         ArrayList<Rule> returnList = new ArrayList<>();
-        returnList.add(new Rule("\\babstract\\b", "KW_ABSTRACT"));
-        returnList.add(new Rule("\\bassert\\b", "KW_ASSERT"));
-        returnList.add(new Rule("\\bboolean\\b", "KW_BOOLEAN"));
-        returnList.add(new Rule("\\bbreak\\b", "KW_BREAK"));
-        returnList.add(new Rule("\\bbyte\\b", "KW_BYTE"));
-        returnList.add(new Rule("\\bcase\\b", "KW_CASE"));
-        returnList.add(new Rule("\\bcatch\\b", "KW_CATCH"));
-        returnList.add(new Rule("\\bchar\\b", "KW_CHAR"));
-        returnList.add(new Rule("\\bclass\\b", "KW_CLASS"));
-        returnList.add(new Rule("\\bconst\\b", "KW_CONST"));
-        returnList.add(new Rule("\\bcontinue\\b", "KW_CONTINUE"));
-        returnList.add(new Rule("\\bdefault\\b", "KW_DEFAULT"));
-        returnList.add(new Rule("\\bdo\\b", "KW_DO"));
-        returnList.add(new Rule("\\bdouble\\b", "KW_DOUBLE"));
-        returnList.add(new Rule("\\belse\\b", "KW_ELSE"));
-        returnList.add(new Rule("\\benmu\\b", "KW_ENUM"));
-        returnList.add(new Rule("\\bextends\\b", "KW_EXTENDS"));
-        returnList.add(new Rule("\\bfinal\\b", "KW_FINAL"));
-        returnList.add(new Rule("\\bfinally\\b", "KW_FINALLY"));
-        returnList.add(new Rule("\\bfloat\\b", "KW_FLOAT"));
-        returnList.add(new Rule("\\bfor\\b", "KW_FOR"));
-        returnList.add(new Rule("\\bgoto\\b", "KW_GOTO"));
-        returnList.add(new Rule("\\bif\\b", "KW_IF"));
-        returnList.add(new Rule("\\bimplements\\b", "KW_IMPLEMENTS"));
-        returnList.add(new Rule("\\bimport\\b", "KW_IMPORT"));
-        returnList.add(new Rule("\\binstanceof\\b", "KW_INSTANCEOF"));
-        returnList.add(new Rule("\\bint\\b", "KW_INT"));
-        returnList.add(new Rule("\\binterface\\b", "KW_INTERFACE"));
-        returnList.add(new Rule("\\blong\\b", "KW_LONG"));
-        returnList.add(new Rule("\\bnative\\b", "KW_NATIVE"));
-        returnList.add(new Rule("\\bnew\\b", "KW_NEW"));
-        returnList.add(new Rule("\\bpackage\\b", "KW_PACKAGE"));
-        returnList.add(new Rule("\\bprivate\\b", "KW_PRIVATE"));
-        returnList.add(new Rule("\\bprotected\\b", "KW_PROTECTED"));
-        returnList.add(new Rule("\\bpublic\\b", "KW_PUBLIC"));
-        returnList.add(new Rule("\\breturn\\b", "KW_RETURN"));
-        returnList.add(new Rule("\\bshort\\b", "KW_SHORT"));
-        returnList.add(new Rule("\\bstatic\\b", "KW_STATIC"));
-        returnList.add(new Rule("\\bstrictfp\\b", "KW_STRICTFP"));
-        returnList.add(new Rule("\\bsuper\\b", "KW_SUPER"));
-        returnList.add(new Rule("\\bswitch\\b", "KW_SWITCH"));
-        returnList.add(new Rule("\\bsynchronized\\b", "KW_SYNCHRONIZED"));
-        returnList.add(new Rule("\\bthis\\b", "KW_THIS"));
-        returnList.add(new Rule("\\bthrow\\b", "KW_THROW"));
-        returnList.add(new Rule("\\bthrows\\b", "KW_THROWS"));
-        returnList.add(new Rule("\\btransient\\b", "KW_TRANSIENT"));
-        returnList.add(new Rule("\\btry\\b", "KW_TRY"));
-        returnList.add(new Rule("\\bvoid\\b", "KW_VOID"));
-        returnList.add(new Rule("\\bvolatile\\b", "KW_VOLATILE"));
-        returnList.add(new Rule("\\bwhile\\b", "KW_WHILE"));
-        returnList.add(new Rule("\\bfalse\\b", "KW_FALSE"));
-        returnList.add(new Rule("\\bnull\\b", "KW_NULL"));
-        returnList.add(new Rule("\\btrue\\b", "KW_TRUE"));
-        returnList.add(new Rule("\\s+", "SC_WHITESPACE"));
-        returnList.add(new Rule("\".+\"", "LI_STRING"));
-        returnList.add(new Rule("\'.+\'", "LI_CHAR"));
-        returnList.add(new Rule("//.*", "CO_ONE_LINE"));
-        returnList.add(new Rule("/\\*([^*]|[\\r\\n]|(\\*+([^*/]|[\\r\\n])))*\\*+/", "CO_MULTI_LINE"));
+        returnList.add(new Rule("\\b(abstract|assert|boolean|break|byte|case|catch|char|class|const|continue" +
+                "|default|do|double|else|enmu|extends|final|finally|float|for|goto|if|implements|import|instanceof|int" +
+                "|interface|long|native|new|package|private|protected|public|return|short|static|strictfp|super|switch" +
+                "|synchronized|this|throw|throws|transient|try|void|volatile|while|false|null|true)\\b", "KW"));
+        returnList.add(new Rule("\"[^\\\"]+\"|'[^\\\"]+'", "ST"));
+        returnList.add(new Rule("//.*|/\\*([^*]|[\\r\\n]|(\\*+([^*/]|[\\r\\n])))*\\*+/", "CO"));
         return returnList;
+
+
     }
 }
